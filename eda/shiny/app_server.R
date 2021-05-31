0b934e07e04b73ca11879308b334bed2cb952be0
Daniel Miau <dmiau88@uw.edu>
2021-05-31 12:03:06 -0700

    shiny files

---
+++ eda/shiny/app_server.R
@@ -0,0 +1,13 @@
+# load your libraries
+# read in the data set 
+
+# Clean your data and explore variables 
+
+# Define the server
+server <- function(input, output) {
+  
+  # further clean and/or manipulate the data based on the input from the widgets
+  # any code that has input$ or output$ (ex. Your chart or a dataframe that will updated based on user input 
+  # insert code for chart here        
+  
+}
